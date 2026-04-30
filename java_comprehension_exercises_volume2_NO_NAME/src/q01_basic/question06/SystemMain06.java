13bde743d720e96c2f60fc913ab5dae37855c0c6
ykamimu <ykamimu@C010423151.zipangu.cac.co.jp>
2026-04-30 12:30:22 +0900

    q01_basic.question06まで完了

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question06/SystemMain06.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question06/SystemMain06.java
@@ -1,5 +1,7 @@
 package q01_basic.question06;
 
+import java.io.IOException;
+
 public class SystemMain06 {
 
 	public static void main(String[] args) {
@@ -13,7 +15,28 @@ public class SystemMain06 {
 
 		System.out.println("---CREATE MEMBER DATA---");
 		//TODO ここから実装する
+		System.out.print("input id>>");
+		try {
+			inputId = cr.inputNumber();
+		} catch (NumberFormatException e) {
+			System.out.println("error");
+			e.printStackTrace();
+			return;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
 
+		System.out.print("input password>>");
+		try {
+			inputPassword = cr.inputString();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		Member member = new Member(inputId, inputPassword, name, age, rank);
+		member.showMember();
 	}
 
 }
